34c8eaad52b7904431b527e3394188abd4e7156d
FacundoC222 <facuuundo2017@gmail.com>
2025-04-14 18:17:05 -0300

    Add files via upload

---
+++ Ejercicio 5/Ejercicio 5.py
@@ -0,0 +1,72 @@
+#Ejercicio 1
+#def imprimir_hola_mundo():
+#    print("Hola mundo")
+#imprimir_hola_mundo()
+
+#Ejercicio 2
+#def saludar_usuario(nombre):
+#    print("Hola", nombre , "!")
+#nombre=input("Ingrese su nombre: ")
+#saludar_usuario(nombre)
+
+#Ejercicio 3
+#def informacion_personal(nombre,apellido,edad,residencia):
+#    print(f"Soy ",nombre,apellido,", tengo ",edad," años y vivo en ",residencia)
+#nombre=input("Ingrese su nombre: ")
+#apellido=input("Ingrese su apellido: ")
+#edad=input("Ingrese su edad: ")
+#residencia=input("Ingrese su residencia: ")
+#informacion_personal(nombre,apellido,edad,residencia)
+
+#Ejercicio 4
+#def calcular_area_circulo(radio):
+#    print(f"El area del cirulo es ", 3.14*(radio**2))
+#def calcular_perimetro_circulo(radio):
+#    print(f"El perimetro del circulo es ", 2*3.14*radio)
+#radio=int(input("Ingrese el radio del circulo: "))
+#calcular_area_circulo(radio)
+#calcular_perimetro_circulo(radio)
+
+#Ejercicio 5
+#def segundos_a_horas(segundos):
+#    print(f"Esos segundos son ", segundos/3600, " horas")
+#segundos=int(input("Ingrese los segundos: "))
+#segundos_a_horas(segundos)
+
+#Ejercicio 6
+#def tabla_multiplicar(numero):
+#    cont=0
+#    while cont!=11:
+#        print(cont*numero)
+#        cont+=1
+#numero=int(input("Ingrese su numero para ver la tabla de multiplicacion: "))
+#tabla_multiplicar(numero)
+
+#Ejercicio 7
+#def operaciones_basicas(a, b):
+#    resultados=(a+b,a-b,a*b,a/b)
+#    print("El resultado de la suma es ",resultados[0]," el de la resta es ",resultados[1]," el de la multiplicacion es ",resultados[2]," y el de la divicion es ",resultados[3])
+#a=int(input("Ingrese el primer numero: "))
+#b=int(input("Ingrese el segundo numero: "))
+#operaciones_basicas(a,b)
+
+#Ejercicio 8
+#def calcular_imc(peso, altura):
+#    print("Su IMC es de ", peso/(altura**2))
+#peso=int(input("Ingrese su peso en KG: "))
+#altura=float(input("Ingrese su altura en metros: "))
+#calcular_imc(peso,altura)
+
+#Ejercicio 9
+#def celsius_a_fahrenheit(celsius):
+#    print("La temperatura en fahrenheit es ", (celsius*9/5)+32)
+#celsius=float(input("Ingrese la temperatura en celcius: "))
+#celsius_a_fahrenheit(celsius)
+
+#Ejercicio 10
+#def calcular_promedio(a, b, c):
+#    print("El promedio es ",(a+b+c)/3)
+#a=int(input("Ingrese el primer numero: "))
+#b=int(input("Ingrese el segundo numero: "))
+#c=int(input("Ingrese el tercer numero: "))
+#calcular_promedio(a, b, c)
