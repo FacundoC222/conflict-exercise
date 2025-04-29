d65a6f8f9d9205635b510ad872ceb2ffdd0c28fa
FacundoC222 <facuuundo2017@gmail.com>
2025-04-29 18:27:09 -0300

    Add files via upload

---
+++ ejecicio 6/Ejercicio 6.py
@@ -0,0 +1,51 @@
+#Ejercicio 1
+#lista=list(range(4,100,4))
+#print(lista)
+
+#Ejercicio 2
+#lista=[1,2,3,4,5]
+#print(lista[-2])
+
+#Ejercicio 3
+#lista=[]
+#lista.append("Messi")
+#lista.append("Churrasco")
+#lista.append("Casa")
+#print(lista)
+
+#Ejercicio 4
+#animales = ["perro", "gato", "conejo", "pez"]
+#animales[-1]="oso"
+#animales[-2]="loro"
+#print(animales)
+
+#Ejercicio 5
+#El codigo se encarga de buscar el numero mas alto dentro de la lista, y una vez encontrado se remueve de la misma.
+
+#Ejercicio 6
+#lista=list(range(10,30,5))
+#print(lista[0],lista[1])
+
+#Ejercicio 7
+#autos=["sedan", "polo", "suran", "gol"]
+#autos[1]="Moto"
+#autos[2]="Camion"
+#print(autos)
+
+#Ejercicio 8
+#dobles=[]
+#dobles.append(5*2)
+#dobles.append(10*2)
+#dobles.append(15*2)
+#print(dobles)
+
+#Ejercicio 9
+#compras = [["pan", "leche"], ["arroz", "fideos", "salsa"],["agua"]]
+#compras[2].append("jugo")
+#compras[1][1]="tallarines"
+#compras[0].remove("pan")
+#print(compras)
+
+#Ejercicio 10
+#lista_anidada=[15,[True],[25.5,57.9,30.6],[False]]
+#print(lista_anidada)
